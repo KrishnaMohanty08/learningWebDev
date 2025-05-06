ff6aba40514c863b85e0c4aab5f6738ff9dc390a
Sessobin <mohantykrishna57@gmail.com>
2025-05-06 19:16:59 +0530

    passOP

---
+++ MongoDB/PassOP-mongo/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Navbar from './components/navBar'
+import Manager from './components/Manager'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="bg-black h-screen">
+    <Navbar/>
+    <Manager/>
+    </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ MongoDB/PassOP-mongo/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const navBar = () => {
+  return (
+    <nav className='bg-green-600 flex justify-around items-center align-bottom my-auto text-lg py-2'>
+        
+        <div className='logo font-bold gap-9 justify-between'>
+        <span className="text-red-600">&lt;</span>
+        <span className='text-white'>Pass<span className='text-black'>OP</span></span>
+        <span className='text-red-600'>/&gt;</span>
+        </div>
+
+      <ul>
+        <li className='flex gap-9 justify-between'>
+            <a className='hover:font-semibold hover:text-white ' href="#">Home</a>
+            <a className='hover:font-semibold hover:text-white' href="#">Contact</a>
+            <a className='hover:font-semibold hover:text-white' href="#">About</a>
+        </li>
+      </ul>
+      <a className='cursor-pointer' href="https://github.com/KrishnaMohanty08/learningWebDev/tree/main/React/">
+        <img className='w-8 px--8' src="src/components/icons/github.png"/>
+      </a>
+    </nav>
+  )
+}
+
+export default navBar
